72ac734d6bed278d63bbe27cadd8d554066ed283
Manuel Strebel <manuel.strebel@novu.ch>
2021-07-18 19:18:44 +0200

    initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,71 @@
+export default {
+  target: 'server',
+  ssr: false,
+
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  head: {
+    title: 'i18n-test',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  // Global CSS (https://go.nuxtjs.dev/config-css)
+  css: [],
+
+  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+  plugins: [],
+
+  // Auto import components (https://go.nuxtjs.dev/config-components)
+  components: true,
+
+  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+    // https://go.nuxtjs.dev/tailwindcss
+    '@nuxtjs/tailwindcss',
+  ],
+
+  // Modules (https://go.nuxtjs.dev/config-modules)
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    'nuxt-i18n',
+  ],
+
+  // Axios module configuration (https://go.nuxtjs.dev/config-axios)
+  axios: {},
+
+  i18n: {
+    defaultLocale: 'de',
+    seo: false,
+    langDir: 'lang/',
+    strategy: 'prefix',
+    detectBrowserLanguage: {
+      useCookie: true,
+      cookieKey: 'language',
+      onlyOnNoPrefix: true,
+    },
+    locales: [
+      {
+        code: 'de',
+        iso: 'de-DE',
+        name: 'Deutsch',
+        file: 'de.js',
+      },
+      {
+        code: 'en',
+        iso: 'en-US',
+        name: 'Englisch',
+        file: 'en.js',
+      },
+    ],
+  },
+
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  build: {},
+}
---
+++ pages/index.vue
@@ -0,0 +1,66 @@
+<template>
+  <div
+    class="
+      container
+      mx-auto
+      flex flex-col
+      justify-center
+      min-h-screen
+      items-center
+    "
+  >
+    <div class="flex">
+      <nuxt-link
+        v-for="locale in availableLocales"
+        :key="locale.code"
+        :to="switchLocalePath(locale.code)"
+        >{{ locale.name }}</nuxt-link
+      >
+    </div>
+    <div>
+      <Logo />
+      <h1 class="title">{{ $t('welcome') }}</h1>
+      <h2 class="title">{{ $i18n.locale }}</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    availableLocales() {
+      return this.$i18n.locales.filter((i) => i.code !== this.$i18n.locale)
+    },
+  },
+}
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+@apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
